fix sk1, use hessian approx method tune alpha and rho

sk1.py
@@ -7,57 +7,41 @@ import numpy as np
 
 class Sk1(LineSearch):
     def __init__(self, f: func.Function, name: str, start_point: np.ndarray = None, norm: Union[str, float] = 2,
-                 eps: float = 10 ** -6, max_iterations: int = 10 ** 6, initial_alpha: float = 1, rho: float = 0.99,
+                 eps: float = 10 ** -6, max_iterations: int = 10 ** 6, initial_alpha: float = 0.3, rho: float = 0.8,
                  c: float = 0.99):
         super().__init__(f, name, start_point, norm, eps, max_iterations, initial_alpha, rho, c)
 
         # initalize bk as identity matrix
-        self.Bk = np.eye(self.f.get_dim(), dtype=float)
+        self.H = np.eye(self.f.get_dim(), dtype=float)
 
-    def linesearch(self, dk: np.array):
-        # impl. Armijo-Goldstein line search
-
+    def linesearch(self, dk: np.array, xk: np.array, gk: np.array):
         alpha = self.initial_alpha
-        c = self.c
-        rho = self.rho
-
-        # Get current x value and gradient
-        xk = self.x_k
-        gk = self.f.grad(xk)
-        fk = self.f.evaluate(xk)
-
-        # Perform line search
-        while alpha > self.eps:
-            x_new = xk + alpha * dk
-            fk_new = self.f.evaluate(x_new)
-            if fk_new <= fk + c * alpha * np.dot(gk, dk):
-                return alpha
-            alpha *= rho
-
+        while self.f.evaluate(xk + alpha * dk) > self.f.evaluate(xk) + self.initial_alpha * alpha * np.dot(gk, dk):
+            alpha *= self.rho
         return alpha
 
     def update(self):
-        # get current x value
+        # todo in this implementation of sk1 I use a Hessian approximation
+        #   this might be implemented with approximating a Broyden matrix as well!?!?
+
         xk = self.x_k
         # gradient at pont x
         gk = self.f.grad(xk)
-        # todo maybe replace this solve with sherman-morrison formula
-        dk = -1.0 * np.linalg.solve(self.Bk, gk)
+        dk = -np.dot(self.H, gk)
 
-        # perform linesearch to find step_size
-        sk = self.linesearch(dk)
+        # perform linesearch
+        alpha = self.linesearch(dk, xk, gk)
 
         # update x value
-        self.x_k = xk + sk * dk
-        sk = self.x_k - xk
-        yk = self.f.grad(self.x_k) - self.f.grad(xk)
+        x_new = xk + alpha * dk
+        yk = self.f.grad(x_new) - gk
 
-        ys = yk - np.dot(self.Bk, sk)
-        # todo this line might be more efficient
-        # todo is greater equal really right here?
-        if np.dot(ys.T, sk) > 0:
-            self.Bk = self.Bk - (ys * ys.T) / np.dot(ys.T, sk)
+        s = alpha * dk
+        if np.abs(np.dot(yk, s)) >= self.eps * np.linalg.norm(yk) * np.linalg.norm(s):
+            Hy = np.dot(self.H, yk)
+            self.H += np.outer(s - Hy, s - Hy) / np.dot(yk, s - Hy)
 
+        self.x_k = x_new
         # set stuff for superclass monitoring and update
         self.grad_f_k = gk
         super().update()